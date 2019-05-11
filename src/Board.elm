9d833aa981c58caa9096e933f1696e42cd5cea66
Adam Weider <adamweider98@gmail.com>
2019-05-11 16:55:40 -0500

    premium and tiles

--- src/Board.elm
+++ src/Board.elm
@@ -1,71 +1,103 @@
 module Board exposing (Board, Square, Multiplier, init, view)
 
-import Html exposing (Html)
+import Html exposing (Html, Attribute)
 import Html.Attributes exposing (class)
+
 import Matrix exposing (Matrix)
+import Tile exposing (Tile)
 
 type Multiplier
   = Letter Int
   | Word Int
 
 type Square
-  = Blank
-  | Tile Char
+  = Empty
+  | Tile Tile
   | Premium Multiplier
 
 type Board = B (Matrix Square)
 
 doubleLetters : List (Int, Int)
-doubleLetters = [ (0, 3), (0, 11)
-                , (2, 6), (2, 8)
-                , (3, 0), (3, 7), (3, 14)
-                , (6, 2), (6, 6), (6, 8), (6, 12)
-                , (7, 3), (7, 11)
-                , (8, 2), (8, 6), (8, 8), (8, 12)
-                , (11, 0), (11, 7), (11, 14)
-                , (12, 6), (12, 8)
-                , (14, 3), (14, 11)
-                ]
+doubleLetters =
+  [ (0, 3), (0, 11)
+  , (2, 6), (2, 8)
+  , (3, 0), (3, 7), (3, 14)
+  , (6, 2), (6, 6), (6, 8), (6, 12)
+  , (7, 3), (7, 11)
+  , (8, 2), (8, 6), (8, 8), (8, 12)
+  , (11, 0), (11, 7), (11, 14)
+  , (12, 6), (12, 8)
+  , (14, 3), (14, 11)
+  ]
+
+tripleLetters : List (Int, Int)
+tripleLetters =
+  [ (1, 5), (1, 9)
+  , (5, 1), (5, 5), (5, 9), (5, 13)
+  , (9, 1), (9, 5), (9, 9), (9, 13)
+  , (13, 5), (13, 9)
+  ]
+
+doubleWords : List (Int, Int)
+doubleWords =
+  [ (1, 1), (1, 13)
+  , (2, 2), (2, 12)
+  , (3, 3), (3, 11)
+  , (4, 4), (4, 10)
+  , (7, 7)
+  , (10, 4), (10, 10)
+  , (11, 3), (11, 11)
+  , (12, 2), (12, 12)
+  , (13, 1), (13, 13)
+  ]
+
+tripleWords : List (Int, Int)
+tripleWords =
+  [ (0, 0), (0, 7), (0, 14)
+  , (7, 0), (7, 14)
+  , (14, 0), (14, 7), (14, 14)
+  ]
 
 setByIndices : List (Int, Int) -> a -> Matrix a -> Matrix a
 setByIndices is x matrix = List.foldl (\(i, j) m -> Matrix.set i j x m) matrix is
 
 init : Board
 init =
-  Blank
+  Empty
     |> Matrix.repeat 15 15
     |> setByIndices doubleLetters (Premium (Letter 2))
+    |> setByIndices tripleLetters (Premium (Letter 3))
+    |> setByIndices doubleWords (Premium (Word 2))
+    |> setByIndices tripleWords (Premium (Word 3))
     |> B
 
-className : Square -> String
-className square =
-  case square of
-    Blank  -> "blank"
-    Tile _ -> "tile"
-    Premium mult ->
+viewEmpty : Html msg
+viewEmpty =
+  Html.div
+  [ class "empty" ]
+  []
+
+viewPremium : Multiplier -> Html msg
+viewPremium mult =
+  let
+    (num, kind, str) =
       case mult of
-        Letter i -> "premium-letter-" ++ String.fromInt i
-        Word i   -> "premium-word-" ++ String.fromInt i
+        Letter i -> (String.fromInt i, "letter", "x LS")
+        Word i   -> (String.fromInt i, "word", "x WS")
+  in
+    Html.div
+    [ class ("premium-" ++ kind ++ "-" ++ num) ]
+    [ Html.text (num ++ str) ]
 
 viewSquare : Square -> Html msg
 viewSquare square =
-  let
-    (text, name) =
-      case square of
-        Blank  -> ([], "blank")
-        Tile c -> ([String.fromChar c], "tile")
-        Premium mult ->
-          case mult of
-            Letter i ->
-              let num = String.fromInt i in
-              ([num ++ "x LS"], "premium-letter-" ++ num)
-            Word i   ->
-              let num = String.fromInt i in
-              ([num ++ "x WS"], "premium-word-" ++ num)
-  in
-    Html.div
-      [ class "square", class name]
-      (List.map Html.text text)
+  case square of
+    Empty  ->
+      viewEmpty
+    Tile tile ->
+      Tile.view tile
+    Premium mult ->
+      viewPremium mult
 
 view : Board -> Html msg
 view (B matrix) =
---
+++ src/Tile.elm
@@ -0,0 +1,55 @@
+module Tile exposing (..)
+
+import Html exposing (Html, Attribute)
+import Html.Attributes exposing (class)
+
+type Tile
+  = Blank
+  | Letter Char Int
+
+points : List (Int, List Char)
+points =
+  [ ( 1,  [ 'E', 'A', 'I', 'O', 'N', 'R', 'T', 'L', 'S', 'U' ] )
+  , ( 2,  [ 'D', 'G' ] )
+  , ( 3,  [ 'B', 'C', 'M', 'P' ] )
+  , ( 4,  [ 'F', 'H', 'V', 'W', 'Y' ] )
+  , ( 5,  [ 'K' ] )
+  , ( 8,  [ 'J', 'X' ] )
+  , ( 10, [ 'Q', 'Z' ] )
+  ]
+
+-- to avoid redundant case expression i guess
+value : Char -> Int
+value =
+  let
+    check map c =
+      case map of
+        [] -> 0
+        (p, cs)::rest ->
+          if List.member c cs then
+            p
+          else
+            check rest c
+  in
+    check points
+
+blank : Tile
+blank = Blank
+
+letter : Char -> Tile
+letter c = Letter c (value c)
+
+view : Tile -> Html msg
+view tile =
+  let
+    html =
+      case tile of
+        Blank -> []
+        Letter c i ->
+          [ Html.div [ class "letter" ] [ Html.text (String.fromChar c) ]
+          , Html.div [ class "points" ] [ Html.text (String.fromInt i) ]
+          ]
+  in
+    Html.div
+      [ class "tile" ]
+      html
