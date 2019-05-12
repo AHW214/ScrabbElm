87b0a6676be4a7b736448daec883684a7493292e
Troy Hu <hut@cs.uchicago.edu>
2019-05-12 15:12:02 -0500

    adding debug functions for RedBlackTree

--- src/RedBlackTree.elm
+++ src/RedBlackTree.elm
@@ -72,3 +72,81 @@ balanceRR color l val r =
   case (color, (l, val, r)) of
     (B, (a, x, T R b y (T R c z d))) -> T R (T B a x b) y (T B c z d)
     _                                -> T color l val r
+
+--------------------------------------------------------------------------------
+--debug helpers from RedBlackTree.elm (lecture)
+
+getColor t = case t of
+  T c _ _ _ -> c
+  E         -> B
+
+root t = case t of
+  T _ _ x _ -> x
+  E         -> Debug.todo "root"
+
+getLeft t = case t of
+  T _ l _ _ -> l
+  E         -> Debug.todo "left"
+
+getRight t = case t of
+  T _ _ _ r -> r
+  E         -> Debug.todo "right"
+
+height t = case t of
+  E         -> 0
+  T _ l _ r -> 1 + max (height l) (height r)
+
+size t = case t of
+  E         -> 0
+  T _ l _ r -> 1 + size l + size r
+
+bso t =
+  let
+    nonDecreasing xs =
+      case xs of
+        x1::x2::rest -> x1 <= x2 && nonDecreasing (x2::rest)
+        _            -> True
+  in
+  nonDecreasing (toList t)
+
+toList : Tree a -> List a
+toList t = case t of
+  E                -> []
+  T _ left x right -> toList left ++ [x] ++ toList right
+
+maybeBlackHeight t = case t of
+  E -> Just 0
+  T c l _ r ->
+    maybeBlackHeight l |> Maybe.andThen (\n ->
+    maybeBlackHeight r |> Maybe.andThen (\m ->
+      if n /= m then Nothing
+      else if c == B then Just (1 + n)
+      else Just n
+    ))
+
+okBlackHeight t =
+  case maybeBlackHeight t of
+    Just _  -> True
+    Nothing -> False
+
+blackHeight t =
+  case maybeBlackHeight t of
+    Just n  -> n
+    Nothing -> Debug.todo "bh"
+
+noRedRed t = case t of
+  E                   -> True
+  T R (T R _ _ _) _ _ -> False
+  T R _ _ (T R _ _ _) -> False
+  T _ l _ r           -> noRedRed l && noRedRed r
+
+oneRedRed t = case t of
+  E                             -> False
+  T R (T R _ _ _) _ (T R _ _ _) -> False
+  T R (T R l1 _ r1) _ r         -> noRedRed l1 && noRedRed r1 && noRedRed r
+  T R l _ (T R l2 _ r2)         -> noRedRed l && noRedRed l2 && noRedRed r2
+  T _ l _ r                     -> False
+
+maybeOneRedRed t = oneRedRed t || noRedRed t
+
+redBlackTree t = bso t && noRedRed t && okBlackHeight t
