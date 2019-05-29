02e05050c9c7b8bb8089633d0f70846a16eb1ae0
Adam Weider <adamweider98@gmail.com>
2019-05-29 18:24:39 -0500

    websocket js [only ignoring compiled js]

---
+++ src/js/webSocket.js
@@ -0,0 +1,106 @@
+// Based on https://github.com/MattCheely/elm-port-examples/blob/master/websocket
+
+function bind(app)
+{
+    if (!app.ports || !(app.ports.toSocket && app.ports.fromSocket))
+    {
+      return;
+    }
+
+    let sockets = {};
+
+    app.ports.toSocket.subscribe(message => {
+      switch (message.msgType)
+      {
+      case "connect":
+        openWebSocket(message.msg);
+        break;
+      case "sendString":
+        sendString(message.msg);
+        break;
+      }
+    });
+
+    function openWebSocket(request)
+    {
+      if (sockets[request.url])
+      {
+        return;
+      }
+
+      let toElm = app.ports.fromSocket;
+      let socket = new WebSocket(request.url, request.protocols);
+
+      socket.onopen = openHandler.bind(null, toElm, socket, request.url);
+      socket.onmessage = messageHandler.bind(null, toElm, socket, request.url);
+      socket.onerror = errorHandler.bind(null, toElm, socket, request.url);
+      socket.onclose = closeHandler.bind(null, toElm, sockets, request.url);
+
+      sockets[request.url] = socket;
+    }
+
+    function sendString(request)
+    {
+      let socket = sockets[request.url];
+      if (socket)
+      {
+        socket.send(request.message);
+      }
+      else
+      {
+        console.log(`No open socket for: ${request.url}. Cannot send ${request.message}`);
+      }
+    }
+}
+
+function messageHandler(toElm, socket, url, event)
+{
+  if (typeof event.data === "string")
+  {
+    toElm.send({
+      msgType: "stringMessage",
+      msg: {
+        url: url,
+        binaryType: socket.binaryType,
+        extensions: socket.extensions,
+        protocol: socket.protocol,
+        data: event.data
+      }
+    });
+  }
+  else
+  {
+    console.log(`Binary message handling not supported.`);
+  }
+}
+
+function errorHandler(toElm, socket, url, event)
+{
+  toElm.send({
+    msgType: "error",
+    msg: {
+      url: url,
+      binaryType: socket.binaryType,
+      extensions: socket.extensions,
+      protocol: socket.protocol,
+      code: event.code
+    }
+  });
+}
+
+function closeHandler(toElm, sockets, url, event)
+{
+  let socket = sockets[url];
+  sockets[url] = undefined;
+
+  toElm.send({
+    msgType: "closed",
+    msg: {
+      url: url,
+      binaryType: socket.binaryType,
+      extensions: socket.extensions,
+      protocol: socket.protocol,
+      unsentBytes: socket.bufferedAmount
+    }
+  });
+}
